050cfa178bb2ccee62294f0cfb7157f07c863f1b
Felipe Oliveira <lipefan0@gmail.com>
2025-09-25 15:48:27 -0300

    initial commit

---
+++ src/test/kotlin/br/com/contis/blingservice/BlingServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.contis.blingservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlingServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
