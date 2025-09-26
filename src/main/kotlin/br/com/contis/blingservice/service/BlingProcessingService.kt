4cb31c65c2c0bb45d81c328b4937a12e4fb7b8ed
Felipe Oliveira <lipefan0@gmail.com>
2025-09-26 11:20:19 -0300

    cria servico para processar a venda e enviar para a fila no rabbitmq

--- src/main/kotlin/br/com/contis/blingservice/service/BlingProcessingService.kt
+++ src/main/kotlin/br/com/contis/blingservice/service/BlingProcessingService.kt
@@ -1,4 +1,47 @@
 package br.com.contis.blingservice.service
 
-class BlingProcessingService {
+import br.com.contis.blingservice.client.BlingApiClient
+import br.com.contis.blingservice.client.dto.order.BlingOrderResponseDTO
+import br.com.contis.blingservice.config.RabbitMqConfig
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class BlingProcessingService(
+    private val blingApiClient: BlingApiClient,
+    private val rabbitTemplate: RabbitTemplate
+) {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    suspend fun processOrderById(orderId: Long): BlingOrderResponseDTO {
+        try {
+            log.info("Iniciando o processamento do pedido com ID: $orderId")
+
+            // Simulação de obtenção da chave da API (pode ser de um banco de dados ou configuração)
+            val apiKey = "sua_chave_api_aqui"
+
+            val orderResponse = blingApiClient.findOrderById(orderId, apiKey)
+
+            log.info("Dados do pedido {} recebidos com sucesso. Número no Bling: {}", orderId, orderResponse.data.numero)
+
+            // --- PRÓXIMOS PASSOS DA LÓGICA ENTRARIAM AQUI ---
+            // Ex: Mapear para um novo evento, publicar no RabbitMQ, etc.
+
+            rabbitTemplate.convertAndSend(
+                RabbitMqConfig.BUSINESS_EVENT_EXCHANGE, // Para qual exchange enviar
+                RabbitMqConfig.ORDER_READY_FOR_FULFILLMENT_ROUTING_KEY, // Qual a "etiqueta" de roteamento
+                orderResponse.data // O objeto a ser enviado (será convertido para JSON)
+            )
+            log.info("Evento para o pedido {} publicado no exchange '{}'", orderId, RabbitMqConfig.BUSINESS_EVENT_EXCHANGE)
+
+
+            // Retornamos os dados que buscamos
+            return orderResponse
+        } catch (e: Exception) {
+            log.error("Falha ao processar o pedido com ID: {}. Causa: {}", orderId, e.message)
+            // Lançar a exceção permite que o chamador decida como lidar com a falha (ex: NACK)
+            throw e
+        }
+    }
 }
